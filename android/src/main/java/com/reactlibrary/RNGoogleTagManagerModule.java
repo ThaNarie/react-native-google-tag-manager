3cbdf19ebacfcca5956bc930fbc1a63012ff2702
Tugkan Kibar <tugkan.kibar@mediamonks.com>
2018-10-17 15:41:30 +0200

    Added initial android

--- android/src/main/java/com/reactlibrary/RNGoogleTagManagerModule.java
+++ android/src/main/java/com/reactlibrary/RNGoogleTagManagerModule.java
@@ -1,13 +1,25 @@
 
 package com.reactlibrary;
 
+import android.os.Bundle;
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class RNGoogleTagManagerModule extends ReactContextBaseJavaModule {
 
+  private FirebaseAnalytics mFirebaseAnalytics = null;
   private final ReactApplicationContext reactContext;
 
   public RNGoogleTagManagerModule(ReactApplicationContext reactContext) {
@@ -18,5 +30,31 @@ public class RNGoogleTagManagerModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNGoogleTagManager";
+  }
+
+  @ReactMethod
+  public void initialiseFirebase() {
+    mFirebaseAnalytics = FirebaseAnalytics.getInstance(getReactApplicationContext());
+  }
+
+  @ReactMethod
+  public void logEvent(ReadableMap event) {
+    String name = event.getString("name");
+    HashMap<String, Object> data = event.getMap("data").toHashMap();
+
+    if (mFirebaseAnalytics != null) {
+      Bundle bundle = new Bundle();
+
+      Set entrySet = data.entrySet();
+      Iterator iterator = entrySet.iterator();
+
+      while (iterator.hasNext()) {
+        Map.Entry mapEntry = (Map.Entry) iterator.next();
+
+        bundle.putString((String) mapEntry.getKey(), (String) mapEntry.getValue());
+      }
+
+      mFirebaseAnalytics.logEvent(name, bundle);
+    }
   }
 }
